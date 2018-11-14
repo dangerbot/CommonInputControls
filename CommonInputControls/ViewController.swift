1c256cbf1a286ccfa38722deb1d3d32deccfce61
Steve <dangerboy@macbook-pro-3.local>
2018-11-13 20:18:37 -0800

    Added some simple buttons and sliders... works ok.

--- CommonInputControls/ViewController.swift
+++ CommonInputControls/ViewController.swift
@@ -7,16 +7,45 @@
 //
 
 import UIKit
+import os.log
 
 class ViewController: UIViewController {
 
-    @IBAction func myButtonWasTapped(_ sender: Any) {
-    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        os_log("ViewController. viewDidLoad()", log: OSLog.default, type: .info)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func myButtonWasTapped(_ sender: Any) {
+        os_log("ViewController. myButtonWasTapped()", log: OSLog.default, type: .info)
+        print(sender)
+    }
+    
+    @IBAction func mySwitchWasToggled(_ sender: UISwitch) {
+        os_log("ViewController. mySwitchWasToggled()", log: OSLog.default, type: .info)
+        print(sender)
+        if (sender as AnyObject).isOn {
+            print("The switch is on")
+        } else {
+            print("The switch is off")
+        }
+    }
 
-}
+    @IBAction func mySliderValueChanged(_ sender: UISlider) {
+        os_log("ViewController. mySliderValueChanged()", log: OSLog.default, type: .info)
+        print(sender.value)
+    }
+    
+    @IBAction func textFieldReturnKeyTapped(_ sender: UITextField) {
+        os_log("ViewController. textFieldReturnKeyTapped()", log: OSLog.default, type: .info)
+        if let text = sender.text { print(text) }
+    }
+    
+    
+    
+    
+} // END class ViewController
 
