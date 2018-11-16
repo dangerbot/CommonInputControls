9de2eb90667c78feb475fbd6908f96df8fae9ee9
Steve <dangerboy@macbook-pro-3.local>
2018-11-15 20:02:47 -0800

    Added program dependent segue. works!

--- CommonInputControls/ViewController.swift
+++ CommonInputControls/ViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  OrangeViewController.swift
 //  CommonInputControls
 //
 //  Created by Steve on 11/12/18.
@@ -17,16 +17,16 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        os_log("ViewController. viewDidLoad()", log: OSLog.default, type: .info)
+        os_log("OrangeViewController. viewDidLoad()", log: OSLog.default, type: .info)
         // Do any additional setup after loading the view, typically from a nib.
     }
     
     @IBAction func unwindToRed(unwindSegue: UIStoryboardSegue) {
-        os_log("ViewController. unwindToRed(...)", log: OSLog.default, type: .info)
+        os_log("OrangeViewController. unwindToRed(...)", log: OSLog.default, type: .info)
     }
 
     @IBAction func myButtonWasTapped(_ sender: Any) {
-        os_log("ViewController. myButtonWasTapped()", log: OSLog.default, type: .info)
+        os_log("OrangeViewController. myButtonWasTapped()", log: OSLog.default, type: .info)
         print(sender)
         if mySwitch.isOn {
             print("Switch: ON")
@@ -36,8 +36,23 @@ class ViewController: UIViewController {
         print("Slider Value: \(mySlider.value)")
     }
     
+    @IBAction func pinkButtonTapped(_ sender: UIButton) {
+        os_log("OrangeViewController. pinkButtonTapped()", log: OSLog.default, type: .info)
+        if mySwitch.isOn {
+            performSegue(withIdentifier: "PinkSegue", sender: nil)
+        }
+    }
+    
+    @IBAction func greyButtonTapped(_ sender: Any) {
+        os_log("OrangeViewController. pinkButtonTapped()", log: OSLog.default, type: .info)
+        if mySwitch.isOn {
+            performSegue(withIdentifier: "GreySegue", sender: nil)
+        }
+    }
+    
+    
     @IBAction func mySwitchWasToggled(_ sender: UISwitch) {
-        os_log("ViewController. mySwitchWasToggled()", log: OSLog.default, type: .info)
+        os_log("OrangeViewController. mySwitchWasToggled()", log: OSLog.default, type: .info)
         print(sender)
         if (sender as AnyObject).isOn {
             print("The switch is on")
@@ -47,23 +62,23 @@ class ViewController: UIViewController {
     }
 
     @IBAction func mySliderValueChanged(_ sender: UISlider) {
-        os_log("ViewController. mySliderValueChanged()", log: OSLog.default, type: .info)
+        os_log("OrangeViewController. mySliderValueChanged()", log: OSLog.default, type: .info)
         print(sender.value)
     }
     
     @IBAction func textFieldReturnKeyTapped(_ sender: UITextField) {
-        os_log("ViewController. textFieldReturnKeyTapped()", log: OSLog.default, type: .info)
+        os_log("OrangeViewController. textFieldReturnKeyTapped()", log: OSLog.default, type: .info)
         if let text = sender.text { print(text) }
     }
     
     @IBAction func myRespondToTapGesture(_ sender: UITapGestureRecognizer) {
-        os_log("ViewController. textFieldReturnKeyTapped()", log: OSLog.default, type: .info)
+        os_log("OrangeViewController. textFieldReturnKeyTapped()", log: OSLog.default, type: .info)
         let location = sender.location(in: view)
         print(location)
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        os_log("ViewController. prepare(UIStoryboardSegue)", log: OSLog.default, type: .info)
+        os_log("OrangeViewController. prepare(UIStoryboardSegue)", log: OSLog.default, type: .info)
         segue.destination.navigationItem.title = frontTextField.text
         // segue.destination.pinkLabelText
     }
